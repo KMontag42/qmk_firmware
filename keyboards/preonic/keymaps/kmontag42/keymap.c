f9ef59da715260f5cb375a5b2b0d09cb088ae1c0
Kyle Montag <kmontag@salsify.com>
2018-09-04 09:38:15 -0400

    updates for preonic

--- keyboards/preonic/keymaps/kmontag42/keymap.c
+++ keyboards/preonic/keymaps/kmontag42/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+--------+------|------+--------+------+------+------+-------|
  * | LSPO |   Z  |   X  |   C  |   V    |   B  |   N  |   M    |   ,  |   .  |   /  |MEH/Ent|
  * |------+------+------+------+--------+------+------+--------+------+------+------+-------|
- * | Ctrl | MEH  | Alt  | GUI  |Lower/| | Bspc |  Spc |Raise/} | Left | Down |  Up  |Right  |
+ * | Ctrl | MEH  | Alt  | GUI  |Lower/[ | Bspc |  Spc |Raise/] | Left | Down |  Up  |Right  |
  * `----------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,               KC_T,    KC_Y,    KC_U,               KC_I,     KC_O,    KC_P,    KC_LBRC,  \
   ALL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,               KC_G,    KC_H,    KC_J,               KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSPO,       KC_Z,    KC_X,    KC_C,    KC_V,               KC_B,    KC_N,    KC_M,               KC_COMM,  KC_DOT,  KC_SLSH, MEH_T(KC_ENT), \
-  KC_LCTL,       KC_MEH,  KC_LALT, KC_LGUI, LT(LOWER, KC_PIPE), KC_BSPC, KC_SPC,  LT(RAISE, KC_RCBR), KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT \
+  KC_LCTL,       KC_PIPE,  KC_LALT, KC_LGUI, LT(_LOWER, KC_LBRC), KC_BSPC, KC_SPC,  LT(_RAISE, KC_RBRC), KC_LEFT,  KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Lower
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+  RESET, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Raise
@@ -117,36 +117,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 
 
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-        case QWERTY:
-          if (record->event.pressed) {
-            set_single_persistent_default_layer(_QWERTY);
-          }
-          return false;
-          break;
-        case LOWER:
-          if (record->event.pressed) {
-            layer_on(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_LOWER);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-        case RAISE:
-          if (record->event.pressed) {
-            layer_on(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          } else {
-            layer_off(_RAISE);
-            update_tri_layer(_LOWER, _RAISE, _ADJUST);
-          }
-          return false;
-          break;
-      }
-    return true;
 };
